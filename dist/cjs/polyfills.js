1bc6917a813acfe660c687aa5e1e0d9d74c0e63d
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-24 06:45:25 -0800

    Upgrade dependencies

--- dist/cjs/polyfills.js
+++ dist/cjs/polyfills.js
@@ -3,9 +3,4 @@ require("core-js/actual/array/from");
 require("core-js/actual/map");
 require("core-js/actual/set");
 require("core-js/actual/url");
-
-if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
-  Object.defineProperty(exports.default, '__esModule', { value: true });
-  for (var key in exports) exports.default[key] = exports[key];
-  module.exports = exports.default;
-}
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }

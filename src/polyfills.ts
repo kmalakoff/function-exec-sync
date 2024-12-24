0f3db4213f71b2e5fdd5da653d7c11268c602e11
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 14:07:43 -0800

    Replace core-js polyfills with specialized packages

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,4 +1,4 @@
-require('core-js/actual/array/from');
-require('core-js/actual/map');
-require('core-js/actual/set');
-require('core-js/actual/url');
+if (!Array.from) Array.from = require('array-from');
+require('es6-map/implement');
+require('es6-set/implement');
+if (!global.URL) global.URL = require('core-js/web/url');

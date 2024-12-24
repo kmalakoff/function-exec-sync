3a102b6710fb882a3827dfded095eea7dbb9e8a1
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 14:07:43 -0800

    Replace core-js polyfills with specialized packages

--- test/data/promiseError.js
+++ test/data/promiseError.js
@@ -1,6 +1,4 @@
-if (typeof Promise === 'undefined') {
-  require('core-js/actual/promise');
-}
+if (typeof Promise === 'undefined') global.Promise = require('pinkie-promise');
 
 module.exports = function () {
   return Promise.reject(new Error('boom'));

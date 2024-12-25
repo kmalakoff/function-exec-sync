ab662306d88aeeef1cc8d095d5ffb8ce39953ce3
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 21:28:12 -0800

    Update test data and VS Code launch configuration

--- test/data/promise.js
+++ test/data/promise.js
@@ -1,4 +1,4 @@
-if (typeof Promise === 'undefined') global.Promise = require('pinkie-promise');
+const Promise = require('pinkie-promise');
 
 module.exports = function (value) {
   return Promise.resolve(value);

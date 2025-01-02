9ae8565004e579f8abbba962fe817430451cedc6
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-02 14:32:36 -0800

    Upgrade modules

--- test/data/promiseError.js
+++ test/data/promiseError.js
@@ -1,5 +1,5 @@
-const Promise = require('pinkie-promise');
+const Pinkie = require('pinkie-promise');
 
 module.exports = function () {
-  return Promise.reject(new Error('boom'));
+  return Pinkie.reject(new Error('boom'));
 };

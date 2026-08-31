9b2ad83ce43d8bbc890939791d720aac2a1aa53f
Kevin Malakoff <kmalakoff@gmail.com>
2026-08-31 00:47:00 -0700

    Move existsSync to compat layer and add mkdir/copy helpers

--- src/compat.ts
+++ src/compat.ts
@@ -2,8 +2,18 @@
  * Compatibility Layer for Node.js 0.8+
  * Local to this package - contains only needed functions.
  */
+import fs from 'fs';
 import os from 'os';
 
 export function tmpdir(): string {
   return typeof os.tmpdir === 'function' ? os.tmpdir() : require('os-shim').tmpdir();
+}
+
+export function existsSync(test: string): boolean {
+  try {
+    (fs.accessSync || fs.statSync)(test);
+    return true;
+  } catch (_) {
+    return false;
+  }
 }
